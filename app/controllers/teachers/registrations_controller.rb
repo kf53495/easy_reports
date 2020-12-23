588bfcd82d14bdb031f187740fb231848fe1f5d5
kf53495 <fkmndk902902@gmail.com>
2020-12-23 18:08:29 +0900

    teachersテーブルに登録するときの表示設定を修正

--- app/controllers/teachers/registrations_controller.rb
+++ app/controllers/teachers/registrations_controller.rb
@@ -41,14 +41,14 @@ class Teachers::RegistrationsController < Devise::RegistrationsController
   private
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name,:last_name_kana, :first_name_kana, :profile])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name,:last_name_kana, :first_name_kana, :profile])
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:last_name, :first_name,:last_name_kana, :first_name_kana, :profile])
+  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
