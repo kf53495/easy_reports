c86b8522fcae3aa534d8d6ca3efc8ee17ac3b895
kf53495 <fkmndk902902@gmail.com>
2021-02-03 13:02:38 +0900

    生徒情報を登録できるように設定

--- app/controllers/students/registrations_controller.rb
+++ app/controllers/students/registrations_controller.rb
@@ -46,9 +46,9 @@ class Students::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :students
+  devise_for :students, controllers: {
+    registrations: 'students/registrations'
+  }
   devise_for :teachers, controllers: {
     registrations: 'teachers/registrations'
   }
