45e7945e656f3c4229293c5b3984ae536abc079e
kf53495 <fkmndk902902@gmail.com>
2021-02-03 17:30:28 +0900

    教材を登録できるように設定

--- app/controllers/study_materials_controller.rb
+++ app/controllers/study_materials_controller.rb
@@ -1,8 +1,26 @@
 class StudyMaterialsController < ApplicationController
   def index
   end
+
   def new
+    @study_material = StudyMaterial.new
   end
+
   def create
+    @study_material = StudyMaterial.new(study_material_params)
+    if @study_material.save
+      redirect_to action: 'index'
+    else
+      render :new
+    end
   end
+
+  private
+
+  def study_material_params
+    params.require(:study_material).permit(:name)
+  end
+
+
+
 end
