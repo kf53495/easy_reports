6826161d9f39c310a41e639fdbb8a6aa0b789107
kf53495 <fkmndk902902@gmail.com>
2021-07-30 22:11:31 +0900

    ストロングパラメーターの再修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -47,7 +47,7 @@ class ReportsController < ApplicationController
   private
 
   def report_params
-    params.require(:report).permit(:month, :date, :day, :subject, :time_id, :lesson_type_id, :attendance_status_id, :contact, :homework_status, :quiz, :quiz_result, :quiz_correspondence, :study_material, :other_study_material, :lesson_contents, :lesson_status, :retry_time, :homework, :teacher, :student_id)
+    params.require(:report).permit(:month, :date, :day, :subject, :time, :lesson_type_id, :attendance_status_id, :contact, :homework_status, :quiz, :quiz_result, :quiz_correspondence, :study_material, :other_study_material, :lesson_contents, :lesson_status, :retry_time, :homework, :teacher, :student_id)
   end
 
   def move_to_index
